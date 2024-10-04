01360250c519bdece9293ccc9123e972229cbf48
raffaell95 <raffaell95@live.com>
2024-10-04 17:20:03 -0300

    add 01

--- partials/content.py
+++ partials/content.py
@@ -8,4 +8,48 @@ class MainContent(ft.UserControl):
         self.expand = True
 
     def build(self):
-        return ft.Container()
+        banner = ft.Container(
+            content=ft.Container(
+                content=ft.ResponsiveRow(
+                    columns=12,
+                    vertical_alignment=ft.CrossAxisAlignment.END,
+                    controls=[
+                        ft.Container(
+                            padding=ft.padding.all(50),
+                            content=ft.Column(
+                                ft.Text(value='Descubra meu incrivel portfolio', theme_style=ft.TextThemeStyle.HEADLINE_LARGE),
+                                ft.Text(
+                                    spans=[
+                                        ft.TextSpan(text='<'),
+                                        ft.TextSpan(text='code', style=ft.TextStyle(color=ft.colors.PRIMARY))
+                                    ]
+                                )
+                            )
+                        )
+                    ]
+                )
+            )
+        )
+
+        experience = ft.Container()
+        projects = ft.Container()
+        prices = ft.Container()
+        testimonials = ft.Container()
+        logos = ft.Container()
+        footer = ft.Container()
+
+        return ft.Container(
+            content=ft.Column(
+                controls=[
+                    banner,
+                    experience,
+                    projects,
+                    prices,
+                    testimonials,
+                    logos,
+                    footer
+                ]
+            ),
+            bgcolor=ft.colors.BACKGROUND,
+            padding=ft.padding.all(30)
+        )
