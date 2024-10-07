3fcafb15ce12a94209c13151617403c15057d760
raffaell95 <raffaell95@live.com>
2024-10-07 15:10:54 -0300

    add 6

--- partials/content.py
+++ partials/content.py
@@ -9,6 +9,17 @@ class MainContent(ft.UserControl):
 
     def build(self):
         banner = ft.Container(
+            shadow=ft.BoxShadow(
+                color=ft.colors.ON_BACKGROUND,
+                offset=ft.Offset(x=0, y=60),
+                spread_radius = -30
+            ),
+            image_src= 'images/bg.jpg',
+            image_fit=ft.ImageFit.COVER,
+            image_repeat=ft.ImageRepeat.NO_REPEAT,
+            image_opacity=0.5,
+            bgcolor=ft.colors.BACKGROUND,
+            margin=ft.margin.only(top=30),
             content=ft.Container(
                 content=ft.ResponsiveRow(
                     columns=12,
@@ -17,21 +28,77 @@ class MainContent(ft.UserControl):
                         ft.Container(
                             padding=ft.padding.all(50),
                             content=ft.Column(
-                                ft.Text(value='Descubra meu incrivel portfolio', theme_style=ft.TextThemeStyle.HEADLINE_LARGE),
-                                ft.Text(
-                                    spans=[
-                                        ft.TextSpan(text='<'),
-                                        ft.TextSpan(text='code', style=ft.TextStyle(color=ft.colors.PRIMARY))
-                                    ]
-                                )
+                                controls=[
+                                    ft.Text(value='Descubra meu incrivel portfolio', theme_style=ft.TextThemeStyle.HEADLINE_LARGE),
+                                    ft.Text(
+                                        spans=[
+                                            ft.TextSpan(text='<'),
+                                            ft.TextSpan(text='code', style=ft.TextStyle(color=ft.colors.PRIMARY)),
+                                            ft.TextSpan(text='> '),
+
+                                            ft.TextSpan(
+                                                text='Eu desenvolvo aplicativos ios e Android',
+                                                style=ft.TextStyle(color=ft.colors.WHITE),
+                                            ),
+                                            ft.TextSpan(text='</'),
+                                            ft.TextSpan(text='code', style=ft.TextStyle(color=ft.colors.PRIMARY)),
+                                            ft.TextSpan(text='> '),
+                                        ],
+                                        theme_style=ft.TextThemeStyle.BODY_MEDIUM
+                                    ),
+                                    ft.ElevatedButton(
+                                        bgcolor=ft.colors.PRIMARY,
+                                        content=ft.Text(value='Explore agora', color=ft.colors.BLACK, weight=ft.FontWeight.BOLD),
+                                        url='#',
+                                        style=ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=0)),
+                                    )
+                                ],
+                                spacing=30,
+                                alignment=ft.MainAxisAlignment.CENTER,
+                            )
+                        ),
+                        ft.Container(
+                            col={'md': 12, 'lg': 4},
+                            content=ft.Image(
+                                src='images/face-2.png',
+                                width=20,
+                                # scale=ft.Scale(scale=1.8, alignment=ft.alignment.bottom_center)
                             )
                         )
                     ]
                 )
+            ),
+            
+        )
+
+        experience = ft.Container(
+            content=ft.ResponsiveRow(
+                columns=12,
+                controls=[
+                    ft.Text(
+                        col={'xs': 6, 'md': 3},
+                        spans=[
+                            ft.TextSpan(
+                                text='15 + ',
+                                style=ft.TextStyle(
+                                    color=ft.colors.PRIMARY,
+                                    weight=ft.FontWeight.W_900,
+                                    size=20
+                                )
+                            ),
+                            ft.TextSpan(
+                                text=' Anos de experiência',
+                                style=ft.TextStyle(
+                                    color=ft.colors.WHITE,
+                                    size=16
+                                )
+                            )
+                        ]
+                    )
+                ]
             )
         )
 
-        experience = ft.Container()
         projects = ft.Container()
         prices = ft.Container()
         testimonials = ft.Container()
@@ -41,7 +108,7 @@ class MainContent(ft.UserControl):
         return ft.Container(
             content=ft.Column(
                 controls=[
-                    banner,
+                   # banner,
                     experience,
                     projects,
                     prices,
